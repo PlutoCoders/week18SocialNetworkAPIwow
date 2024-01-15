60990c4604da346891b2a58cd6993b0eeaaec409
Alux <plutocoding@gmail.com>
2024-01-15 08:42:28 -0500

    setting up model files, index reaction thought user js

---
+++ models/Thought.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'Please add a thought',
+      minlength: 1,
+      maxlength: 150
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
