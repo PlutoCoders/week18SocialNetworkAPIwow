b030dc4c28a489886add5a317545cb7c68a7981b
Alux <plutocoding@gmail.com>
2024-01-15 16:54:23 -0500

    controller - getting user, creating user, creating thought, getting thought

---
+++ controllers/thought-controller.js
@@ -0,0 +1,60 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+  // get all thoughts
+  async getThoughts(req, res) {
+    try {
+      const dbThoughtData = await Thought.find()
+        .sort({ createdAt: -1 });
+
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  // get single thought by id
+  async getSingleThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOne({ _id: req.params.thoughtId });
+
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  // create a thought
+  async createThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.create(req.body);
+
+      const dbUserData = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: dbThoughtData._id } },
+        { new: true }
+      );
+
+      let successMessage = `Thought created!`;
+      let noUserIDFoundMessage = `Can't find this id, but the thought is created`;
+
+      let message = { message: successMessage, thought: dbThoughtData };
+
+      if (!dbUserData) {
+        message = { message: noUserIDFoundMessage, help: `Cant find this id!` };
+        return res.status(404).json(message); 
+      };
+
+      res.json(message);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+}
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,47 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+  async getUsers(req, res) {
+    try {
+      const dbUserData = await User.find()
+        .select('-__v')
+        .populate('friends')
+        .populate('thoughts');
+
+      res.json(dbUserData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  async getSingleUser(req, res) {
+    try {
+      const dbUserData = await User.findOne({ _id: req.params.userId })
+        .select('-__v')
+        .populate('friends')
+        .populate('thoughts');
+
+      if (!dbUserData) {
+        return res.status(404).json({ message: 'Cant find this id.' });
+      }
+
+      res.json(dbUserData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  async createUser(req, res) {
+    try {
+      const dbUserData = await User.create(req.body);
+      res.json({ message: `User Created!`, user: dbUserData });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+}
+
+module.exports = userController;
