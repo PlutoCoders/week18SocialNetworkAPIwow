b1aee3c7acd676f2fae526d726e3ec7d292f67cd
Alux <plutocoding@gmail.com>
2024-01-15 08:19:12 -0500

    setting up generic boiler server and index js

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/socialbook`);
+const db = mongoose.connection;
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+
+    getUsers();
+  });
+});
